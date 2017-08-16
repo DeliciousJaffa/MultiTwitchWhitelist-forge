99fdf866be2c0b3fd3ce10c759f6e1cc3643dbdd
Jaffa <Jaffa@DeliciousJaffa.co.uk>
2017-08-16 04:07:24 +0100

    Bump version numbers for dev/next version

--- src/main/java/cat/jaffa/multitwitchwhitelist/forge/MultiTwitchWhitelist.java
+++ src/main/java/cat/jaffa/multitwitchwhitelist/forge/MultiTwitchWhitelist.java
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 public class MultiTwitchWhitelist {
     public static final String MODNAME = "MultiTwitchWhitelist";
     public static final String MODID = "multitwitchwhitelist-forge";
-    public static final String VERSION = "0.2";
+    public static final String VERSION = "0.3";
 
     @Instance(MODID)
     public static MultiTwitchWhitelist instance;
